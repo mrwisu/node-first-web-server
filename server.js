aa8e18f26ec3f98ca14a901608a41408394c6f61
Wojciech Wisowaty <mrwisu@gmail.com>
2018-05-06 00:58:50 +0200

    Installed XO - lint and fixed errors it found in server.js

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
 
-var app = express();
+const app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
+  fs.appendFile('server.log', log + '\n', err => {
     if (err) {
       console.log(err);
     }
-  })
+  });
   next();
 });
 
-app.use((req, res, next) => {
-  var now = new Date().toString();
+app.use((req, res) => {
   res.render('maintenance.hbs', {
     pageTitle: 'Under construction',
     welcomeText: 'Comming back soon...'
-  })
+  });
 });
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text) => {
+hbs.registerHelper('screamIt', text => {
   return text.toUpperCase();
 });
 
@@ -40,12 +41,12 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeText: 'Welcome on Home Page'
-  })
+  });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
   });
 });
 
