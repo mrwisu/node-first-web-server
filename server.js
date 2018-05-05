2f2fda7b25a9290caf462dfc4320a2681304f47b
Wojciech Wisowaty <mrwisu@gmail.com>
2018-05-05 20:48:32 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+  next();
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  res.render('maintenance.hbs', {
+    pageTitle: 'Under construction',
+    welcomeText: 'Comming back soon...'
+  })
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeText: 'Welcome on Home Page'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad request'
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT);
+console.log(`The server is up on port ${PORT}`);
